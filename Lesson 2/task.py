2129f761c856da0743857f7a2c7adaff15b543e7
Abdurasul Ergashev <abdurasul15.10.1998@gmail.com>
2025-07-20 21:29:22 +0500

    Variables

---
+++ Lesson 2/task.py
@@ -0,0 +1,9 @@
+"""Variables"""
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+
+print(f"""Salom, men {name}.
+Men {age} yoshdaman va {city}da yashayman.""")
