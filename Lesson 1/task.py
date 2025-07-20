14b1eea304e24b9f7c0c0171d6842ffbe5b76b26
Abdurasul Ergashev <abdurasul15.10.1998@gmail.com>
2025-07-20 20:35:53 +0500

    print and input

---
+++ Lesson 1/task.py
@@ -0,0 +1,6 @@
+"""Print() and Input()"""
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Salom, {name}. Siz {age} yoshdasiz. Yana 10 yildan so'ng siz {10+age} yoshda bo'lasiz")
