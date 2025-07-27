d689985b7eccc044e63f410535adb5bb62d7935d
Abdurasul Ergashev <abdurasul15.10.1998@gmail.com>
2025-07-27 15:27:01 +0500

    learn function done

---
+++ Lesson 10/task.py
@@ -0,0 +1,19 @@
+num1 = int(input("Num1: "))
+num2 = int(input("Num2: "))
+
+
+def calculator(number1: int,number2: int):
+    yegindisi = number1 + number2
+    print(yegindisi)
+    ayirmasi = number1 - number2
+    print(ayirmasi)
+    kopaytmasi = number1 * number2
+    print(kopaytmasi)
+    if number2 != 0:
+        bolinmasi = number1 / number2
+        print(bolinmasi)
+    else:
+        print('0 ga bolinmaydi')
+    print("Hisoblash yakunlandi")
+
+calculator(num1,num2)
